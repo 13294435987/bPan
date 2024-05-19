745ff728f8b8bbdb731a38db7c7f88c61188ab31
huajibin <hujiabin@thunisoft.com>
2024-05-19 11:32:13 +0800

    feat: 单文件上传-补充

--- b-pan-backend/framework/storage-engine/storage-engine-fastdfs/src/main/java/one/baymax/storage/engine/fastdfs/FastDfsStorageEngine.java
+++ b-pan-backend/framework/storage-engine/storage-engine-fastdfs/src/main/java/one/baymax/storage/engine/fastdfs/FastDfsStorageEngine.java
@@ -1,6 +1,9 @@
 package one.baymax.storage.engine.fastdfs;
 
+import java.io.IOException;
 import onem.baymax.pan.storage.core.AbstractStorageEngine;
+import onem.baymax.pan.storage.core.context.DeleteFileContext;
+import onem.baymax.pan.storage.core.context.StoreFileContext;
 
 /**
  * fastdfs存储引擎实现
@@ -8,5 +11,13 @@ import onem.baymax.pan.storage.core.AbstractStorageEngine;
  * @author hujiabin wrote in 2024/5/15 23:45
  */
 public class FastDfsStorageEngine extends AbstractStorageEngine {
+
+    @Override protected void doStore(StoreFileContext context) throws IOException {
+
+    }
+
+    @Override protected void doDelete(DeleteFileContext context) throws IOException {
+
+    }
 
 }
--- b-pan-backend/framework/storage-engine/storage-engine-oss/src/main/java/onem/baymax/pan/storage/engine/oss/OssStorageEngine.java
+++ b-pan-backend/framework/storage-engine/storage-engine-oss/src/main/java/onem/baymax/pan/storage/engine/oss/OssStorageEngine.java
@@ -1,6 +1,9 @@
 package onem.baymax.pan.storage.engine.oss;
 
+import java.io.IOException;
 import onem.baymax.pan.storage.core.AbstractStorageEngine;
+import onem.baymax.pan.storage.core.context.DeleteFileContext;
+import onem.baymax.pan.storage.core.context.StoreFileContext;
 
 /**
  * oss存储引擎实现
@@ -8,5 +11,13 @@ import onem.baymax.pan.storage.core.AbstractStorageEngine;
  * @author hujiabin wrote in 2024/5/15 23:42
  */
 public class OssStorageEngine extends AbstractStorageEngine {
+
+    @Override protected void doStore(StoreFileContext context) throws IOException {
+
+    }
+
+    @Override protected void doDelete(DeleteFileContext context) throws IOException {
+
+    }
 
 }
